2edcf47b70f06b0ce0a558f35a0c9e9ba5d6fb24
TommyTam2012 <tommytam2012@gmail.com>
2025-05-16 10:37:46 +0700

    Update viewer.js

--- pages/api/viewer.js
+++ pages/api/viewer.js
@@ -1,1 +1,18 @@
+export default async function handler(req, res) {
+  const response = await fetch('https://firm-imp-16671.upstash.io/keys/log:*', {
+    headers: {
+      Authorization: 'Bearer AUEfAAIjcDFkMTBkNTFmYmIzM2I0ZGQwYTUzODk5NDI2YmZkNTMwZHAxMA',
+    },
+  });
+
+  const keysData = await response.json();
+
+  const logs = (keysData.result || []).map((key) => {
+    const [_, email, ts] = key.split(':');
+    const timestamp = new Date(Number(ts)).toISOString();
+    return { email, timestamp };
+  });
+
+  res.status(200).json({ logs });
+}
 
